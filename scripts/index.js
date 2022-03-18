fd961d496ff6688f674cf684bbfca44ea1c82335
Daria <daryanickulina@yandex.ru>
2022-03-19 00:39:20 +0300

    feat: loading initial cards with JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-let popup = document.querySelector('.popup');
-let closePopup = document.querySelector('.popup__close');
-let openPopup = document.querySelector('.profile__edit');
+const popup = document.querySelector('.popup');
+const closePopup = document.querySelector('.popup__close');
+const openPopup = document.querySelector('.profile__edit');
 
-let form = document.querySelector('.popup__container');
+const form = document.querySelector('.popup__container');
 
-let name = form.querySelector('input[name="name"]');
-let info = form.querySelector('input[name="description"]');
+const name = form.querySelector('input[name="name"]');
+const info = form.querySelector('input[name="description"]');
 
-let profileHeader = document.querySelector('.profile__header');
-let profileInfo = document.querySelector('.profile__description');
+const profileHeader = document.querySelector('.profile__header');
+const profileInfo = document.querySelector('.profile__description');
 
 function popupShow() {
   popup.classList.toggle('popup_opened');
@@ -34,3 +34,51 @@ closePopup.addEventListener('click', function () {
 });
 
 form.addEventListener('submit', preventSubmit);
+
+
+
+
+
+function renderCard(name, link) {
+  const elements__gallery = document.querySelector('.elements__gallery');
+  const card = document.querySelector('#card').content.firstElementChild.cloneNode(true);
+
+  card.querySelector('.elements__photo').setAttribute('src', link);
+  card.querySelector('.elements__text').textContent = name;
+
+  elements__gallery.append(card);
+}
+
+window.addEventListener('load', function () {
+
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  initialCards.forEach(function (element) {
+    renderCard(element.name, element.link);
+  })
+})
