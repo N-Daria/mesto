9ba8000f3ee728738307f169321aa9c82bd37a78
Daria <daryanickulina@yandex.ru>
2022-05-12 23:49:19 +0300

    feat: webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import '../pages/index.css'; 
+
 import { openEdit, popupEdit, name, info, elementsGallery, openAdd, popupAdd, place, link, initialCards, settings, formsList } from './consts.js';
 import Card from './Card.js';
 import FormValidator from './FormValidator.js'
@@ -9,6 +11,7 @@ import UserInfo from './UserInfo.js'
 
 const editFormValidation = new FormValidator(settings, formsList[0]);
 const addFormValidation = new FormValidator(settings, formsList[1]);
+
 const userInformation = new UserInfo(name, info);
 
 const popupAddClass = new Popup(popupAdd);
@@ -60,6 +63,11 @@ function handleCardClick() {
   bigPhoto.open(this._link, this._name);
 }
 
+function createCard(data) {
+  const photoCard = new Card({ data }, handleCardClick);
+  return photoCard
+}
+
 openAdd.addEventListener('click', function () {
   addFormValidation.enableValidation();
   popupAddClass.open();
@@ -70,8 +78,3 @@ openEdit.addEventListener('click', changeHeader);
 window.addEventListener('load', function () {
   section.renderItems();
 });
-
-function createCard(data) {
-  const photoCard = new Card({ data }, handleCardClick);
-  return photoCard
-}
